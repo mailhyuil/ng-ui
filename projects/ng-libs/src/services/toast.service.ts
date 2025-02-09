3be295bcf95ad11a22d66ea76e8cc75879085903
mailhyuil <mailhyuil@gmail.com>
2025-02-09 20:18:03 +0900

    fix: Update snack-bar duration

--- projects/ng-libs/src/services/toast.service.ts
+++ projects/ng-libs/src/services/toast.service.ts
@@ -1,28 +1,32 @@
-import { Injectable, inject } from "@angular/core";
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { Injectable, inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ToastService {
   snackBar = inject(MatSnackBar);
   openSuccess(message: string) {
-    return this.snackBar.open(message, "확인", {
-      panelClass: ["snackbar-success"],
+    return this.snackBar.open(message, '확인', {
+      panelClass: ['snackbar-success'],
+      duration: 2500,
     });
   }
   openDanger(message: string) {
-    return this.snackBar.open(message, "확인", {
-      panelClass: ["snackbar-danger"],
+    return this.snackBar.open(message, '확인', {
+      panelClass: ['snackbar-danger'],
+      duration: 2500,
     });
   }
   openWarning(message: string) {
-    return this.snackBar.open(message, "확인", {
-      panelClass: ["snackbar-warning"],
+    return this.snackBar.open(message, '확인', {
+      panelClass: ['snackbar-warning'],
+      duration: 2500,
     });
   }
   openPrimary(message: string) {
-    return this.snackBar.open(message, "확인", {
-      panelClass: ["snackbar-primary"],
+    return this.snackBar.open(message, '확인', {
+      panelClass: ['snackbar-primary'],
+      duration: 2500,
     });
   }
 }
